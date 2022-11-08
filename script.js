0a4a2aa367ec6835920f1ccde7ce93c5b7d6b444
Kyubs1610 <diego.decort@gmail.com>
2022-11-08 16:35:00 +0100

    last committed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "01732a6d070744c131e57da3a8bfe38d",
+    apiKey: "3d79f7133d6e9f01769c48deb4697f04",
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -21,7 +21,6 @@ let weather = {
       
       for(i = 0; i<5; i++){
       const  name  = document.querySelector(".bar").value;
-      
       const { description } = data.list[i].weather;
       const { temp, humidity } = data.list[i].main;
       const { speed } = data.list[i].wind;
@@ -38,38 +37,107 @@ let weather = {
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
       }
-      
-    },
+
+      //Chart
+let myChart = null
+
+ var xyValues = [
   
-    search: function () {
+  {x:1, y:(data.list[0].main.temp)},
+  {x:2, y:(data.list[1].main.temp)},
+  {x:3, y:(data.list[2].main.temp)},
+  {x:4, y:(data.list[3].main.temp)},
+  {x:5, y:(data.list[4].main.temp)},
+]
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const config = {
+   type: 'bar',
+   data: {
+       labels: ["1", "2", "3", "4", "5"], 
+       datasets: [{
+           label: '°C',
+           data: xyValues, 
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255, 99, 132, 1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+       }]
+   },
+   options: {
+       scales: {
+           y: {
+               beginAtZero: true
+           }
+       }
+           
+       
+   }
+  
+   
+  
+ };
+
+ if (Chart.getChart("myChart")){
+  Chart.getChart("myChart").destroy();
+}
+
+myChart=new Chart(ctx,config)
+
+    },
+      search: function () {
       this.fetchWeather(document.querySelector(".bar").value);
       
       const  data  = document.querySelector(".bar").value;
-      localStorage.setItem('lastWeather', JSON.stringify(data) );
-      
+      JSON.parse(localStorage.setItem("lastWeather",(data)));
 
     },
   
-
   };
 
-  document.querySelector(".searchBtn").addEventListener("click", function () {
+  document.querySelector(".searchBtn").addEventListener("click", function (event) {
     weather.search();
-  });
+     
+ });
   
   document.querySelector(".bar").addEventListener("keyup", function (event) {
       if (event.key == "Enter") {
         weather.search();
+        
       }
     });
 
-    weather.fetchWeather("Brussels")
 
+// getting the last city knowed from the Local storage    
+
+    window.addEventListener('load', () => {
+      const name2 = localStorage.getItem('lastWeather');
+      document.querySelector(".bar").value = name2;
+      
+  })
+  const name2 = localStorage.getItem('lastWeather'); 
+ 
+  weather.fetchWeather(name2)
  
 
-  //Getting and displaying the text for the upcoming five days of the week
+//Getting and displaying the text for the upcoming five days of the week
 var d = new Date();
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+
 
 function CheckDay(day){
     if(day + d.getDay() > 6){
@@ -79,11 +147,9 @@ function CheckDay(day){
         return day + d.getDay();
     }
 }
-
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)]
+          
     }
 
-
-
-  
+    
